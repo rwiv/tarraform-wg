e1076ca254a7008dfced5668f66c4efaa4caf69a
rwiv <zc798546@gmail.com>
2023-12-24 22:46:02 +0900

    refactor: add modules

--- main.tf
+++ main.tf
@@ -1,81 +1,30 @@
 terraform {
-  required_providers {
-    vultr = {
-      source = "vultr/vultr"
-      version = "2.18.0"
+    required_providers {
+        vultr = {
+            source = "vultr/vultr"
+            version = "2.18.0"
+        }
     }
-  }
 }
 
 provider "vultr" {
-  api_key = var.vultr_api_key
-  rate_limit = 100
-  retry_limit = 3
+    api_key = var.vultr_api_key
+    rate_limit = 100
+    retry_limit = 3
 }
 
 resource "vultr_ssh_key" "wg_ssh_key" {
-  name = "wg-ssh-key"
-  ssh_key = file("./asset/ssh-keygen.pub")
+    name = "wg-ssh-key"
+    ssh_key = file("./asset/ssh-keygen.pub")
 }
 
-resource "vultr_firewall_group" "wg_firewall_group" {
-    description = "wg firewall group"
+module "firewall" {
+    source = "./modules/firewall"
+    my_public_ip = var.my_public_ip
 }
 
-resource "vultr_firewall_rule" "ssh_firewall_rule" {
-    firewall_group_id = vultr_firewall_group.wg_firewall_group.id
-    protocol = "tcp"
-    ip_type = "v4"
-    subnet = var.my_public_ip
-    subnet_size = 32
-    port = "22"
-}
-
-resource "vultr_firewall_rule" "udp_firewall_rule" {
-    firewall_group_id = vultr_firewall_group.wg_firewall_group.id
-    protocol = "udp"
-    ip_type = "v4"
-    subnet = "0.0.0.0"
-    subnet_size = 0
-    port = "51820"
-}
-
-resource "vultr_firewall_rule" "tcp_firewall_rule" {
-    firewall_group_id = vultr_firewall_group.wg_firewall_group.id
-    protocol = "tcp"
-    ip_type = "v4"
-    subnet = "0.0.0.0"
-    subnet_size = 0
-    port = "51821"
-}
-
-resource "vultr_instance" "my_instance" {
-  plan = "vc2-1c-1gb"
-  region = "icn"
-  os_id = 1743
-  ssh_key_ids = [vultr_ssh_key.wg_ssh_key.id]
-  firewall_group_id = vultr_firewall_group.wg_firewall_group.id
-
-  provisioner "local-exec" {
-    command = "echo http://${self.main_ip}:51821 > wg_url.txt"
-  }
-
-  connection {
-    type     = "ssh"
-    user     = "root"
-    private_key  = file("./asset/ssh-keygen")
-    host     = self.main_ip
-  }
-
-  provisioner "file" {
-    source      = "./asset/script.sh"
-    destination = "/root/script.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /root/script.sh",
-      "/root/script.sh ${self.main_ip}",
-    ]
-  }
+module "instance" {
+    source = "./modules/instance"
+    ssh_key_ids = [vultr_ssh_key.wg_ssh_key.id]
+    firewall_group_id = module.firewall.group_id
 }
---
+++ modules/instance/variables.tf
@@ -0,0 +1,7 @@
+variable "ssh_key_ids" {
+    type = list(string)
+}
+
+variable "firewall_group_id" {
+    type = string
+}
